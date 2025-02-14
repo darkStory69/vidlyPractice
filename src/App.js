770dbcddb39cdf8060cb8807d0e15b066280152a
rahulsingh-99 <rahul.rock.rr99@gmail.com>
2025-02-14 20:53:02 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import "./App.css";
+import Customers from "./components/customers";
 import Movies from "./components/movies";
+import {
+  Route,
+  Routes,
+  BrowserRouter as Router,
+  Navigate,
+} from "react-router-dom";
+import NotFound from "./components/notFound";
+import Rentals from "./components/rentals";
+import React, { Component } from "react";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
 
-function App() {
-  return (
-    <div className="container">
-      <Movies />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+          <NavBar />
+          <main className="container">
+            <Routes>
+              <Route path="/movies/:id" element={<MovieForm />} />
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/rentals" element={<Rentals />} />
+              <Route path="/not-found" element={<NotFound />} />
+              <Route path="/" element={<Navigate to="/movies" />} />
+              <Route path="*" element={<Navigate to="/not-found" />} />
+            </Routes>
+          </main>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/movieForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const MovieForm = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  return (
+    <>
+      <h1>MovieForm {id}</h1>
+      <button className="btn btn-primary" onClick={() => navigate("/movies")}>
+        Save
+      </button>
+    </>
+  );
+};
+
+export default MovieForm;
